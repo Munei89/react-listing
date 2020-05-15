4049f7c2b8c20026a76bb4df64a449e793c0140f
Munei Nengwenani <munei.nengwenani@invictuscapital.com>
2020-05-15 18:14:43 +0200

    adding starter kit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import '@babel/polyfill'
+import App from './App'
+import './styles.scss'
+import DefaultErrorBoundary from '../DefaultErrorBoundary'
+
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDom, 1000)
+}
+
+ReactDom.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+
+  document.getElementById('app')
+)
---
+++ webpack.config.base.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_module/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+}
